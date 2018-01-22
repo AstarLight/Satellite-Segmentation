d80bce89458c787530655941e005d154ed4f96af
AstarLight <lijunshi2015@163.com>
2018-01-22 11:19:20 +0800

    add unet

---
+++ unet/gen_dataset.py
@@ -0,0 +1,97 @@
+#coding=utf-8
+
+import cv2
+import random
+import os
+import numpy as np
+from tqdm import tqdm
+
+img_w = 256  
+img_h = 256  
+
+image_sets = ['1.png','2.png','3.png','4.png','5.png']
+
+def gamma_transform(img, gamma):
+    gamma_table = [np.power(x / 255.0, gamma) * 255.0 for x in range(256)]
+    gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
+    return cv2.LUT(img, gamma_table)
+
+def random_gamma_transform(img, gamma_vari):
+    log_gamma_vari = np.log(gamma_vari)
+    alpha = np.random.uniform(-log_gamma_vari, log_gamma_vari)
+    gamma = np.exp(alpha)
+    return gamma_transform(img, gamma)
+    
+
+def rotate(xb,yb,angle):
+    M_rotate = cv2.getRotationMatrix2D((img_w/2, img_h/2), angle, 1)
+    xb = cv2.warpAffine(xb, M_rotate, (img_w, img_h))
+    yb = cv2.warpAffine(yb, M_rotate, (img_w, img_h))
+    return xb,yb
+    
+def blur(img):
+    img = cv2.blur(img, (3, 3));
+    return img
+
+def add_noise(img):
+    for i in range(200): #添加点噪声
+        temp_x = np.random.randint(0,img.shape[0])
+        temp_y = np.random.randint(0,img.shape[1])
+        img[temp_x][temp_y] = 255
+    return img
+    
+    
+def data_augment(xb,yb):
+    if np.random.random() < 0.25:
+        xb,yb = rotate(xb,yb,90)
+    if np.random.random() < 0.25:
+        xb,yb = rotate(xb,yb,180)
+    if np.random.random() < 0.25:
+        xb,yb = rotate(xb,yb,270)
+    if np.random.random() < 0.25:
+        xb = cv2.flip(xb, 1)  # flipcode > 0：沿y轴翻转
+        yb = cv2.flip(yb, 1)
+        
+    if np.random.random() < 0.25:
+        xb = random_gamma_transform(xb,1.0)
+        
+    if np.random.random() < 0.25:
+        xb = blur(xb)
+    
+    if np.random.random() < 0.2:
+        xb = add_noise(xb)
+        
+    return xb,yb
+
+def creat_dataset(image_num = 50000, mode = 'original'):
+    print('creating dataset...')
+    image_each = image_num / len(image_sets)
+    g_count = 0
+    for i in tqdm(range(len(image_sets))):
+        count = 0
+        src_img = cv2.imread('./data/src/' + image_sets[i])  # 3 channels
+        label_img = cv2.imread('./data/road_label/' + image_sets[i],cv2.IMREAD_GRAYSCALE)  # single channel
+        X_height,X_width,_ = src_img.shape
+        while count < image_each:
+            random_width = random.randint(0, X_width - img_w - 1)
+            random_height = random.randint(0, X_height - img_h - 1)
+            src_roi = src_img[random_height: random_height + img_h, random_width: random_width + img_w,:]
+            label_roi = label_img[random_height: random_height + img_h, random_width: random_width + img_w]
+            if mode == 'augment':
+                src_roi,label_roi = data_augment(src_roi,label_roi)
+            
+            visualize = np.zeros((256,256)).astype(np.uint8)
+            visualize = label_roi *50
+            
+            cv2.imwrite(('./unet_train/visualize/%d.png' % g_count),visualize)
+            cv2.imwrite(('./unet_train/road/src/%d.png' % g_count),src_roi)
+            cv2.imwrite(('./unet_train/road/label/%d.png' % g_count),label_roi)
+            count += 1 
+            g_count += 1
+
+
+            
+    
+
+if __name__=='__main__':  
+    creat_dataset(mode='augment')
---
+++ unet/unet_train.py
@@ -0,0 +1,339 @@
+#coding=utf-8
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import argparse
+import numpy as np  
+from keras.models import Sequential  
+from keras.layers import Conv2D,MaxPooling2D,UpSampling2D,BatchNormalization,Reshape,Permute,Activation,Input  
+from keras.utils.np_utils import to_categorical  
+from keras.preprocessing.image import img_to_array  
+from keras.callbacks import ModelCheckpoint  
+from sklearn.preprocessing import LabelEncoder  
+from keras.models import Model
+from keras.layers.merge import concatenate
+from PIL import Image  
+import matplotlib.pyplot as plt  
+import cv2
+import random
+import os
+from tqdm import tqdm  
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+seed = 7  
+np.random.seed(seed)  
+  
+#data_shape = 360*480  
+img_w = 256  
+img_h = 256  
+#有一个为背景  
+#n_label = 4+1  
+n_label = 1
+  
+classes = [0. ,  1.,  2.,   3.  , 4.]  
+  
+labelencoder = LabelEncoder()  
+labelencoder.fit(classes)  
+
+image_sets = ['1.png','2.png','3.png']
+
+def creat_dataset(image_num = 900):
+    print('creating dataset...')
+    image_each = image_num / len(image_sets)
+    g_count = 0
+    for i in range(len(image_sets)):
+        count = 0
+        src_img = cv2.imread('./data/src/' + image_sets[i])  # 3 channels
+        label_img = cv2.imread('./data/label/' + image_sets[i],cv2.IMREAD_GRAYSCALE)  # single channel
+        X_height,X_width,_ = src_img.shape
+        while count < image_each:
+            random_width = random.randint(0, X_width - img_w - 1)
+            random_height = random.randint(0, X_height - img_h - 1)
+            src_roi = src_img[random_height: random_height + img_h, random_width: random_width + img_w,:]
+            label_roi = label_img[random_height: random_height + img_h, random_width: random_width + img_w,:]
+            cv2.imwrite(('./train/src/%d.png',g_count),src_roi)
+            cv2.imwrite(('./train/label/%d.png',g_count),label_roi)
+            count += 1 
+            g_count += 1
+        
+    
+
+def load_img(path, grayscale=False):
+    if grayscale:
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+    else:
+        img = cv2.imread(path)
+        img = np.array(img,dtype="float") / 255.0
+    return img
+
+
+filepath ='./unet_train/'  
+
+def get_train_val(val_rate = 0.25):
+    train_url = []    
+    train_set = []
+    val_set  = []
+    for pic in os.listdir(filepath + 'src'):
+        train_url.append(pic)
+    random.shuffle(train_url)
+    total_num = len(train_url)
+    val_num = int(val_rate * total_num)
+    for i in range(len(train_url)):
+        if i < val_num:
+            val_set.append(train_url[i]) 
+        else:
+            train_set.append(train_url[i])
+    return train_set,val_set
+
+# data for training  
+def generateData(batch_size,data=[]):  
+    #print 'generateData...'
+    while True:  
+        train_data = []  
+        train_label = []  
+        batch = 0  
+        for i in (range(len(data))): 
+            url = data[i]
+            batch += 1 
+            #print (filepath + 'src/' + url)
+            #img = load_img(filepath + 'src/' + url, target_size=(img_w, img_h))  
+            img = load_img(filepath + 'src/' + url)
+            img = img_to_array(img) 
+            # print img
+            #print img.shape  
+            train_data.append(img)  
+            #label = load_img(filepath + 'label/' + url, target_size=(img_w, img_h),grayscale=True)
+            label = load_img(filepath + 'label/' + url, grayscale=True)
+            #label = img_to_array(label).reshape((img_w * img_h,))  
+            label = img_to_array(label)
+            #print label.shape  
+            train_label.append(label)  
+            if batch % batch_size==0: 
+                #print 'get enough bacth!\n'
+                train_data = np.array(train_data)  
+                train_label = np.array(train_label)  
+                #train_label = np.array(train_label).flatten()  
+                #train_label = labelencoder.transform(train_label)  
+                #train_label = to_categorical(train_label, num_classes=n_label)  
+                #train_label = train_label.reshape((batch_size,img_w * img_h,n_label))  
+                yield (train_data,train_label)  
+                train_data = []  
+                train_label = []  
+                batch = 0  
+ 
+# data for validation 
+def generateValidData(batch_size,data=[]):  
+    #print 'generateValidData...'
+    while True:  
+        valid_data = []  
+        valid_label = []  
+        batch = 0  
+        for i in (range(len(data))):  
+            url = data[i]
+            batch += 1  
+            #img = load_img(filepath + 'src/' + url, target_size=(img_w, img_h))
+            img = load_img(filepath + 'src/' + url)
+            #print img
+            #print (filepath + 'src/' + url)
+            img = img_to_array(img)  
+            # print img.shape  
+            valid_data.append(img)  
+            #label = load_img(filepath + 'label/' + url, target_size=(img_w, img_h),grayscale=True)
+            label = load_img(filepath + 'label/' + url, grayscale=True)
+            #label = img_to_array(label).reshape((img_w * img_h,))  
+            label = img_to_array(label)
+            # print label.shape  
+            valid_label.append(label)  
+            if batch % batch_size==0:  
+                valid_data = np.array(valid_data)  
+                valid_label = np.array(valid_label)  
+                #valid_label = np.array(valid_label).flatten()  
+                #valid_label = labelencoder.transform(valid_label)  
+                #valid_label = to_categorical(valid_label, num_classes=n_label)  
+                #valid_label = valid_label.reshape((batch_size,img_w * img_h,n_label))  
+                yield (valid_data,valid_label)  
+                valid_data = []  
+                valid_label = []  
+                batch = 0  
+  
+def SegNet():  
+    model = Sequential()  
+    #encoder  
+    model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(3,img_w,img_h),padding='same',activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(MaxPooling2D(pool_size=(2,2)))  
+    #(128,128)  
+    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(MaxPooling2D(pool_size=(2, 2)))  
+    #(64,64)  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(MaxPooling2D(pool_size=(2, 2)))  
+    #(32,32)  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(MaxPooling2D(pool_size=(2, 2)))  
+    #(16,16)  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(MaxPooling2D(pool_size=(2, 2)))  
+    #(8,8)  
+    #decoder  
+    model.add(UpSampling2D(size=(2,2)))  
+    #(16,16)  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(UpSampling2D(size=(2, 2)))  
+    #(32,32)  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(UpSampling2D(size=(2, 2)))  
+    #(64,64)  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(UpSampling2D(size=(2, 2)))  
+    #(128,128)  
+    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(UpSampling2D(size=(2, 2)))  
+    #(256,256)  
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), input_shape=(3,img_w, img_h), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
+    model.add(BatchNormalization())  
+    model.add(Conv2D(n_label, (1, 1), strides=(1, 1), padding='same'))  
+    model.add(Reshape((n_label,img_w*img_h)))  
+    #axis=1和axis=2互换位置，等同于np.swapaxes(layer,1,2)  
+    model.add(Permute((2,1)))  
+    model.add(Activation('softmax'))  
+    model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])  
+    model.summary()  
+    return model  
+  
+def unet():
+    inputs = Input((3, img_w, img_h))
+
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(256, (3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(pool4)
+    conv5 = Conv2D(512, (3, 3), activation="relu", padding="same")(conv5)
+
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=1)
+    conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(up6)
+    conv6 = Conv2D(256, (3, 3), activation="relu", padding="same")(conv6)
+
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv3], axis=1)
+    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(up7)
+    conv7 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv7)
+
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv2], axis=1)
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(up8)
+    conv8 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv8)
+
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv1], axis=1)
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(up9)
+    conv9 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv9)
+
+    conv10 = Conv2D(n_label, (1, 1), activation="sigmoid")(conv9)
+    #conv10 = Conv2D(n_label, (1, 1), activation="softmax")(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+    model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+  
+def train(args): 
+    EPOCHS = 10
+    BS = 16
+    #model = SegNet()  
+    model = unet()
+    modelcheck = ModelCheckpoint(args['model'],monitor='val_acc',save_best_only=True,mode='max')  
+    callable = [modelcheck]  
+    train_set,val_set = get_train_val()
+    train_numb = len(train_set)  
+    valid_numb = len(val_set)  
+    print ("the number of train data is",train_numb)  
+    print ("the number of val data is",valid_numb)
+    H = model.fit_generator(generator=generateData(BS,train_set),steps_per_epoch=train_numb//BS,epochs=EPOCHS,verbose=1,  
+                    validation_data=generateValidData(BS,val_set),validation_steps=valid_numb//BS,callbacks=callable,max_q_size=1)  
+
+    # plot the training loss and accuracy
+    plt.style.use("ggplot")
+    plt.figure()
+    N = EPOCHS
+    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+    plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy on U-Net Satellite Seg")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig(args["plot"])
+
+  
+
+def args_parse():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--data", help="training data's path",
+                    default=True)
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to output model")
+    ap.add_argument("-p", "--plot", type=str, default="plot.png",
+                    help="path to output accuracy/loss plot")
+    args = vars(ap.parse_args()) 
+    return args
+
+
+if __name__=='__main__':  
+    #creat_dataset()
+    args = args_parse()
+    filepath = args['data']
+    train(args)  
+    #predict()  
